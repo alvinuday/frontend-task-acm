288b5a99e6ebbeaa95a0364f6d3a146fb375119d
Alvin Adarsh <udayalvin@gmail.com>
2021-04-06 11:57:36 +0530

    Submit

---
+++ app.js
@@ -0,0 +1,139 @@
+
+/* <--------------Card-1---------------> */
+var textbox1 = document.getElementsByClassName('textbox1');
+var submit_btn1 = document.getElementsByClassName('submit1');
+var namewrapper1 = document.getElementsByClassName('name-wrapper1');
+var para1 = document.getElementsByClassName('para1');
+var add_comment1 = document.getElementsByClassName('comment1');
+var inner_label1 = document.getElementsByClassName("inner_label1");
+
+
+function click1() {
+    
+    textbox1[0].style.display = "block";
+    submit_btn1[0].style.display = "block";
+    add_comment1[0].style.display = "none";
+
+    console.log("Hello ");
+
+}
+function submit1() {
+    console.log("Bye");
+    inner_label1[0].innerText = textbox1[0].value;
+    textbox1[0].style.display = "none";
+    submit_btn1[0].style.display = "none";
+    para1[0].style.display = "block";
+    namewrapper1[0].style.display = "block";
+
+    
+}
+/* <--------------Card-2---------------> */
+var textbox2 = document.getElementsByClassName('textbox2');
+var submit_btn2 = document.getElementsByClassName('submit2');
+var namewrapper2 = document.getElementsByClassName('name-wrapper2');
+var para2 = document.getElementsByClassName('para2');
+var add_comment2 = document.getElementsByClassName('comment2');
+var inner_label2 = document.getElementsByClassName("inner_label2");
+
+
+function click2() {
+    
+    textbox2[0].style.display = "block";
+    submit_btn2[0].style.display = "block";
+    add_comment2[0].style.display = "none";
+
+    console.log("Hello ");
+
+}
+function submit2() {
+    console.log("Bye");
+    inner_label2[0].innerText = textbox2[0].value;
+    textbox2[0].style.display = "none";
+    submit_btn2[0].style.display = "none";
+    para2[0].style.display = "block";
+    namewrapper2[0].style.display = "block";
+}
+
+
+
+
+
+/* <--------------Card-3---------------> */
+var textbox3 = document.getElementsByClassName('textbox3');
+var submit_btn3 = document.getElementsByClassName('submit3');
+var namewrapper3 = document.getElementsByClassName('name-wrapper3');
+var para3 = document.getElementsByClassName('para3');
+var add_comment3 = document.getElementsByClassName('comment3');
+var inner_label3 = document.getElementsByClassName("inner_label3");
+
+
+function click3() {
+    
+    textbox3[0].style.display = "block";
+    submit_btn3[0].style.display = "block";
+    add_comment3[0].style.display = "none";
+
+    console.log("Hello ");
+
+}
+function submit3() {
+    console.log("Bye");
+    inner_label3[0].innerText = textbox3[0].value;
+    textbox3[0].style.display = "none";
+    submit_btn3[0].style.display = "none";
+    para3[0].style.display = "block";
+    namewrapper3[0].style.display = "block";
+}
+
+/* <--------------Card-4---------------> */
+var textbox4 = document.getElementsByClassName('textbox4');
+var submit_btn4 = document.getElementsByClassName('submit4');
+var namewrapper4 = document.getElementsByClassName('name-wrapper4');
+var para4 = document.getElementsByClassName('para4');
+var add_comment4 = document.getElementsByClassName('comment4');
+var inner_label4 = document.getElementsByClassName("inner_label4");
+
+
+function click4() {
+    
+    textbox4[0].style.display = "block";
+    submit_btn4[0].style.display = "block";
+    add_comment4[0].style.display = "none";
+
+    console.log("Hello ");
+
+}
+function submit4() {
+    console.log("Bye");
+    inner_label4[0].innerText = textbox4[0].value;
+    textbox4[0].style.display = "none";
+    submit_btn4[0].style.display = "none";
+    para4[0].style.display = "block";
+    namewrapper4[0].style.display = "block";
+}
+/* <--------------Card-5---------------> */
+var textbox5 = document.getElementsByClassName('textbox5');
+var submit_btn5 = document.getElementsByClassName('submit5');
+var namewrapper5 = document.getElementsByClassName('name-wrapper5');
+var para5 = document.getElementsByClassName('para5');
+var add_comment5 = document.getElementsByClassName('comment5');
+var inner_label5 = document.getElementsByClassName("inner_label5");
+
+
+function click5() {
+    
+    textbox5[0].style.display = "block";
+    submit_btn5[0].style.display = "block";
+    add_comment5[0].style.display = "none";
+
+    console.log("Hello ");
+
+}
+function submit5() {
+    console.log("Bye");
+    inner_label5[0].innerText = textbox5[0].value;
+    textbox5[0].style.display = "none";
+    submit_btn5[0].style.display = "none";
+    para5[0].style.display = "block";
+    namewrapper5[0].style.display = "block";
+}
